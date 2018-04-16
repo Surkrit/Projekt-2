4b1eedaacc4ae25b44f10c95925ceb769452e698
Vy <amazing.diamondude@gmail.com>
2018-04-16 13:13:47 +0200

    meh js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,14 @@ function add(Username){
     var TheTextBox = document.getElementById("Mytextbox");
     TheTextBox.value = TheTextBox.value + text;
 }
+
+
+function validate(){
+  var confirm = document.getElementById("loginuser");
+  var userName = document.getElementById("loginbutton").value;
+  if ("loginuser" == "Denny123");{
+    alert ("yeah");
+    window.location = "index.html";
+    return false;
+  }
+}
