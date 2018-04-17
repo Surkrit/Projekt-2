c05e12870b84496bdb21e646e96c27761bff46e5
Vy <amazing.diamondude@gmail.com>
2018-04-17 08:29:21 +0200

    Js upload

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -1,48 +1,121 @@
+//var storeSub = document.querySelector("sub, title, dur, year, com");
 
+//function setLocal(localSub)
+//{
+//  var jsonData = JSON.stringify(localSub);
 
-function setLocal(localNotes)
-{
-  var jsonData = JSON.stringify(localNotes);
+//  localStorage.setItem("submission", jsonData);
+//}
 
-  localStorage.setItem("notes", jsonData);
-}
+//function getLocal()
+//{
+//  var notes = localStorage.getItem("submission");
 
-function getLocal()
-{
-  var notes = localStorage.getItem("notes");
+//  if (notes == null){
+//      return [];
+//    }
+//    else {
+//      return JSON.parse(submission);
+//    }
+//}
 
-  if (notes == null)
-    {
-      return [];
-    }
-    else
-    {
-      return JSON.parse(notes);
-    }
-}
+function onloadSub(){
+  var subject = document.getElementById('sub').value;
+  //var uploadFiles = document.getElementById("files");
+  var title = document.getElementById('title').value;
+  var duration = document.getElementById('dur').value;
+  var year = document.getElementById('year').value;
+  var com = document.getElementById('com').value;
 
-function newSub(){
-  var subject = document.querySelector("sub");
-  var uploadFiles = document.querySelector("uploadFiles");
-  var addTitle = document.querySelector("title");
-  var addDuration = document.querySelector("dur");
-  var addYear = document.querySelector("year");
-  var addCom = document.querySelector("com");
-
-  if (sub && uploadfiles && title && dur && year && com){
-    var origSub = getLocal();
-
+  if (subject && title && duration && year && com){
     var uploadArray = {
-      subject : sub,
-      uploadFiles : uploadFiles,
-      addTitle : title,
-      addDuration : dur,
-      addYear : year,
-      addCom : com
+      subject : subject,
+      title : title,
+      duration : dur,
+      year : year,
+      com : com
+    }
+    console.log("does work");
+    globalvalue(uploadArray);
+  } else {
+    console.log("does not work");
+}
+}
 
-    };
-    origSub.push(uploadArray);
-
-    setLocal(origSub);
+function globalvalue(obj) {
+  obj = obj || null;
+  if(obj !== null){
+    var getlocalstorage = localStorage.getItem("contentPost");
+    var mylocal = [];
+    if (getlocalstorage !== null){
+      var mylocal = JSON.parse(getlocalstorage);
+    }
+    mylocal.push(obj);
+    var jsonData = JSON.stringify(mylocal);
+    localStorage.setItem("contentPost", jsonData);
+    showSubject(mylocal)
   }
 }
+
+function showSubject(obj=null) {
+  var divLocation = document.getElementById("adding");
+  divLocation.innerHTML = "";
+  obj = obj || null;
+  if(obj == null) {
+    var localstorage = localstorage.getItem("ContentPost");
+    var obj = JSON.parse(localStorage);
+}
+if(typeof obj !== "undefined" && obj != null) {
+  obj.forEach(function(arg,ii) {
+    if (arg.subject && arg.title && arg.duration && arg.year && arg.com) {
+      var divBox = document.createElement("div");
+
+      var divSub = document.createElement("h1");
+      divSub.innerHTML = arg.subject;
+      divBox.appendChild(divSub);
+
+      var divTitle = document.createElement("h2");
+      divTitle.innerHTML = arg.title;
+      divBox.appendChild(divTitle);
+
+      var divYear = document.createElement("p");
+      divYear.innerHTML = arg.year;
+      divBox.appendChild(divYear);
+
+      divLocation.appendChild(divBox);
+
+    } else {
+      console.log("no onload");
+    }
+    });
+  }
+}
+
+
+window.addEventListener("load",function() {
+
+  var show = window.location.hash;
+  if(show.length <2){
+
+
+    var newButton = document.getElementById("uploadbutton");
+    newButton.addEventListener("click",function(){
+      onloadSub();
+    });
+  }
+});
+
+//    };
+//    console.log("det virker");:
+//    submission.push(uploadArray)
+
+//    setLocal(origSub)
+//  }
+//}
+
+//function buildList()
+//{
+//  var spawnsub = getLocal()
+//  var divSub = document.querySelect("")
+
+//}
